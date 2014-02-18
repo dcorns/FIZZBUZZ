77994ca88d04b9709295e28401aa7dad22294209
Dale Corns <drcorns@hotmail.com>
2014-02-18 02:33:49 -0800

    Add fizzbuzz and fizzbuzz2

---
+++ fizzbuzz.js
@@ -0,0 +1,7 @@
+function FB(num){
+	var result="";
+	if(num%3=0){result='Fizz'}
+		if(num%5){result=result+'Buzz'}
+			if(result==""){result=num}
+				return result;
+}
---
+++ fizzbuzz2.js
@@ -0,0 +1,14 @@
+function FB2(num){
+var FB=new Array();
+function test(num,word){
+	this.num=num;
+	this.word=word;
+}
+var result='';
+FB[0]=new test{3,'Fizz'}; FB[1]=new test{5,'Buzz'}; FB[3]=new test{7,'Sivv'};
+for(var count=0;count<FB.length-1;count++){
+if(FB[count].num%num==0){result=result+FB[count].word}
+	if(result=''){result=num}
+		return result;
+}
+
